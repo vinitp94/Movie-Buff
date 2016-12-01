8c6f985dcbf0eb74bc65712e16c3ceb1537618dd
vinit_parikh <vinitp94@gmail.com>
2016-12-01 10:41:29 -0800

    Complete exercises in 01_queries.rb

--- movie_buff/01_queries.rb
+++ movie_buff/01_queries.rb
@@ -7,6 +7,7 @@ def it_was_ok
   #
   # Find the id, title, and score of all movies with scores between 2 and 3
 
+  Movie.select(:id, :title, :score).where(score: 2..3)
 end
 
 def harrison_ford
@@ -20,6 +21,7 @@ def harrison_ford
   #
   # Find the id and title of all movies in which Harrison Ford appeared but not as a lead actor
 
+  Movie.select(:id, :title).joins(:actors).where("name = 'Harrison Ford'").where("ord != 1")
 end
 
 def biggest_cast
@@ -36,6 +38,7 @@ def biggest_cast
   #
   # Find the id and title of the 3 movies with the largest casts (i.e most actors)
 
+  Movie.select(:id, :title).joins(:actors).group("movies.id").order("COUNT(actors.id) DESC").limit(3)
 end
 
 def directed_by_one_of(them)
@@ -47,6 +50,7 @@ def directed_by_one_of(them)
   #
   # Find the id and title of all the movies directed by one of 'them'.
 
+  Movie.select(:id, :title).joins("JOIN actors ON actors.id = movies.director_id").where("actors.name IN (?)", them)
 end
 
 def movie_names_before_1940
@@ -61,4 +65,5 @@ def movie_names_before_1940
   #
   # Use pluck to find the title of all movies made before 1940.
 
+  Movie.where("yr < 1940").pluck(:title)
 end
